1005d49ff49f94d51067b5bfb8787aaa8bf7b88b
Fernando-Ruiz176 <ruizfe.sud@gmail.com>
2023-09-10 21:53:57 -0300

    Se crea clase Repositorio, se mejora agregar fragmento

---
+++ app/src/main/java/cl/fernando/individual1_m6/Repositorio.kt
@@ -0,0 +1,13 @@
+package cl.fernando.individual1_m6
+
+import androidx.lifecycle.LiveData
+
+class Repositorio (private val tareaDao: TareaDao){
+    suspend fun insertTask(tarea: Tarea){
+        tareaDao.insertarTarea(tarea)
+    }
+    fun getTareas(): LiveData<List<Tarea>> {
+        return tareaDao.getTareas() // obtener tarea
+
+    }
+}
